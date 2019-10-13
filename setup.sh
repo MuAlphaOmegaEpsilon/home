58649d85ebefeac9b8c3d3058e93873a746f20e7
MuAlphaOmegaEpsilon <tommasobonvicin@gmail.com>
2019-10-13 09:32:56 +0000

    Stow the shell package under executables

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set -euf
 # Navigate to the folder containing this file
 cd "$(dirname "$0")"
 
+stow --dir executables shell --target ${HOME}/.local/bin/shell
 stow --dir dotfiles HOME --target ${HOME}
 stow --dir dotfiles XDG_CONFIG_HOME --target ${XDG_CONFIG_HOME}
 
