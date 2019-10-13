c650a256b451a9c1063103f80a386c80cf14ec2f
MuAlphaOmegaEpsilon <tommasobonvicin@gmail.com>
2019-10-13 10:31:27 +0000

    Update setup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@ set -euf
 # Navigate to the folder containing this file
 cd "$(dirname "$0")"
 
+mkdir -p ${HOME}/.local/bin/shell
 stow --dir executables shell --target ${HOME}/.local/bin/shell
+
 stow --dir dotfiles HOME --target ${HOME}
 stow --dir dotfiles XDG_CONFIG_HOME --target ${XDG_CONFIG_HOME}
 
