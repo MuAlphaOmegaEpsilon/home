5654d6691f9c493ee0206410bd319364f2217dc7
MuAlphaOmegaEpsilon <tommasobonvicin@gmail.com>
2019-10-13 11:53:04 +0000

    Edit CONFIG dir name

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ mkdir -p ${HOME}/.local/bin/shell
 stow --dir executables shell --target ${HOME}/.local/bin/shell
 
 stow --dir dotfiles HOME --target ${HOME}
-stow --dir dotfiles XDG_CONFIG_HOME --target ${XDG_CONFIG_HOME}
+stow --dir dotfiles CONFIG --target ${XDG_CONFIG_HOME}
 
-./dotfiles/XDG_CONFIG_HOME/nvim/setup.sh
+./dotfiles/CONFIG/nvim/setup.sh
