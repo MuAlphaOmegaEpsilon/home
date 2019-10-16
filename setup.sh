5bca63d8d638adfc9f96bf977eb619a8ac33eb01
MuAlphaOmegaEpsilon <tommasobonvicin@gmail.com>
2019-10-16 20:47:10 +0100

    Make script shellcheck approved

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ set -euf
 # Navigate to the folder containing this file
 cd "$(dirname "$0")"
 
-mkdir -p ${HOME}/.local/bin/shell
-stow --dir executables shell --target ${HOME}/.local/bin/shell
+mkdir -p "${HOME}"/.local/bin/shell
+stow --dir executables shell --target "${HOME}"/.local/bin/shell
 
-stow --dir dotfiles HOME --target ${HOME}
-stow --dir dotfiles CONFIG --target ${XDG_CONFIG_HOME}
+stow --dir dotfiles HOME --target "${HOME}"
+stow --dir dotfiles CONFIG --target "${XDG_CONFIG_HOME}"
 
 ./dotfiles/CONFIG/nvim/setup.sh
