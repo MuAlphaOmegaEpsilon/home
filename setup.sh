6a18c907f3458d5d0cae8d17501b068f39e7be52
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2019-11-26 20:15:06 +0100

    Update setup script to add dircolors

--- setup.sh
+++ setup.sh
@@ -10,12 +10,14 @@ set +e
 unlink "${HOME}"/developers	2> /dev/null
 unlink "${HOME}"/projects	2> /dev/null
 unlink "${HOME}"/.profile	2> /dev/null
+unlink "${HOME}"/.dircolors	2> /dev/null
 unlink "${XDG_LOCAL_HOME}"	2> /dev/null
 unlink "${XDG_CONFIG_HOME}" 2> /dev/null
 set -e
 
 ln -s -r "${FILE_DIR}"/config						"${XDG_CONFIG_HOME}"
 ln -s -r "${XDG_CONFIG_HOME}"/profile				"${HOME}"/.profile
+ln -s -r "${XDG_CONFIG_HOME}"/dircolors				"${HOME}"/.dircolors
 
 ln -s -r "${FILE_DIR}"/developers					"${XDG_DEVS_HOME}"
 ln -s -r "${XDG_DEVS_HOME}"/MuAlphaOmegaEpsilon		"${HOME}"/projects
