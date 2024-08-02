900279164314ee14ba273359bbb0708f8ca5d5c7
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2024-08-02 20:28:59 +0200

    Add install-packages-new-installation for brand new linux installs

---
+++ .local/shell/install-packages-new-installation.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -euf
+
+sudo apt install bat build-essential cmake curl fd-find ffmpeg fonts-terminus gawk gimp git heif-gdk-pixbuf hexyl htop hwinfo imagemagick libxcb-cursor0 lshw neovim ninja-build plocate telegram-desktop tmux tree
