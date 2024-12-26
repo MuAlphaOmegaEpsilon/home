d8c51571c8caeb0998a885195b3ce225a48c015e
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2024-12-26 23:53:47 +0100

    Update first installation script

--- .local/shell/install-packages-new-installation.sh
+++ .local/shell/install-packages-new-installation.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -euf
 
-sudo apt install bat build-essential cmake curl fd-find ffmpeg fonts-terminus gawk gimp git heif-gdk-pixbuf hexyl htop hwinfo imagemagick libxcb-cursor0 lshw neovim ninja-build plocate telegram-desktop tmux tree
+sudo apt update
+sudo apt upgrade
+sudo apt install bat build-essential cmake curl fd-find ffmpeg fonts-terminus gawk gimp git git-cola heif-gdk-pixbuf hexyl htop hwinfo imagemagick libxcb-cursor0 lshw neovim ninja-build plocate telegram-desktop tmux tree
