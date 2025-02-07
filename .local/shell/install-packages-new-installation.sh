2780627238489feffa65de5bdd1afdec788e3957
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2025-02-07 18:22:03 +0100

    Update script for first debian installation

--- .local/shell/install-packages-new-installation.sh
+++ .local/shell/install-packages-new-installation.sh
@@ -4,4 +4,4 @@ set -euf
 sudo apt update
 sudo apt upgrade
 sudo apt remove firefox-esr
-sudo apt install bat build-essential cmake curl fd-find ffmpeg firefox fonts-terminus gawk gcolor3 gimp git git-cola gnome-shell-extensions gnome-shell-extension-blur-my-shell gnome-shell-extension-caffeine gnome-shell-extension-dashtodock gnome-shell-extension-desktop-icons-ng gnome-shell-extension-tiling-assistant heif-gdk-pixbuf hexyl htop hwinfo imagemagick libxcb-cursor0 lshw neovim ninja-build plocate pass ripgrep telegram-desktop tmux tree
+sudo apt install bat build-essential cmake curl fd-find ffmpeg firefox fonts-terminus gawk gcolor3 gimp git git-cola gnome-shell-extensions gnome-shell-extension-autohidetopbar gnome-shell-extension-blur-my-shell gnome-shell-extension-caffeine gnome-shell-extension-dashtodock gnome-shell-extension-desktop-icons-ng gnome-shell-extension-tiling-assistant heif-gdk-pixbuf hexyl htop hwinfo imagemagick libxcb-cursor0 lshw neovim ninja-build plocate pass ripgrep telegram-desktop tmux tree
